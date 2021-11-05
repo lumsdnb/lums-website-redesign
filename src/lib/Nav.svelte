5a40ca88c91ba12aedcce7ff764eddac67821c2b
lums <jeremy@lums.io>
2021-11-05 21:08:52 +0100

    Add media query navbar

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -2,6 +2,7 @@
 	import { each } from 'svelte/internal';
 	import { page } from '$app/stores';
 	import Clothing from '$lib/clothing.svelte';
+	import MediaQuery from 'svelte-media-query';
 
 	let tabs = [
 		['/', 'lums.io'],
@@ -15,19 +16,49 @@
 	const setActiveTab = (t) => {
 		activeTab = t;
 	};
+
+	let showMobileMenu = false;
+
+	const handleMobileIconClick = () => (showMobileMenu = !showMobileMenu);
 </script>
 
 <nav>
-	<ul>
-		{#each tabs as tab, i}
-			<li
-				class={$page.path == tab[0] ? 'active-tab' : 'inactive-tab'}
-				on:click={(i) => setActiveTab(i)}
-			>
-				<a sveltekit:prefetch href={tab[0]}>{tab[1]}</a>
-			</li>
-		{/each}
-	</ul>
+	<MediaQuery query="(min-width: 583px)" let:matches>
+		{#if matches}
+			<ul class="desktop-nav">
+				{#each tabs as tab, i}
+					<li
+						class={$page.path == tab[0] ? 'active-tab' : 'inactive-tab'}
+						on:click={(i) => setActiveTab(i)}
+					>
+						<a sveltekit:prefetch href={tab[0]}>{tab[1]}</a>
+					</li>
+				{/each}
+			</ul>
+		{:else}
+			<ul class="mobile-nav">
+				<li><a href="/">lums.io</a></li>
+				<li class="hamburger-icon" on:click={handleMobileIconClick}>☰</li>
+			</ul>
+			<ul class="mobile-nav-foldout">
+				{#if showMobileMenu}
+					{#each tabs as tab, i}
+						{#if i !== 0}
+							<li
+								class={$page.path == tab[0] ? 'active-tab' : 'inactive-tab'}
+								on:click={(i) => {
+									setActiveTab(i);
+									handleMobileIconClick;
+								}}
+							>
+								<a sveltekit:prefetch href={tab[0]}>{tab[1]}</a>
+							</li>
+						{/if}
+					{/each}
+				{/if}
+			</ul>
+		{/if}
+	</MediaQuery>
 </nav>
 
 <style>
@@ -40,19 +71,39 @@
 		width: 100%;
 		text-align: center;
 	}
-	ul {
+	.desktop-nav {
 		list-style: none;
 		display: flex;
 		flex-direction: row;
 		justify-content: space-around;
 	}
+	.mobile-nav {
+		list-style: none;
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-end;
+		flex-wrap: wrap;
+	}
+	.mobile-nav-foldout {
+		list-style: none;
+		display: flex;
+		justify-content: flex-end;
+		text-align: right;
+		padding-right: 3rem;
+		flex-wrap: wrap;
+		flex-direction: column;
+	}
+
+	.hamburger-icon {
+		user-select: none;
+	}
 	ul > li {
 		flex: 1;
 		height: 100%;
 	}
-	ul > :nth-child(1) {
+	.mobile-nav > :nth-child(1) {
 		color: #000;
-		flex: 4;
+		flex: 2;
 		text-align: left;
 	}
 	a {
