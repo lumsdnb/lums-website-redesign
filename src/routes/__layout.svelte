e2c46c59fd128c878ac8da29d3777093934dce64
lums <jeremy@lums.io>
2021-11-07 18:41:54 +0100

    Add slick page transitions

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,23 +1,35 @@
-<script>
-	import Nav from '$lib/Nav.svelte';
-	import Footer from '$lib/Footer.svelte';
+<!-- 1. Assign current route's path to `key` prop -->
+<script context="module">
+	export const load = async ({ page }) => ({
+		props: {
+			key: page.path
+		}
+	});
 </script>
 
-<svelte:head>
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Quantico" />
-</svelte:head>
+<script>
+	import '../app.css';
+	import Nav from '$lib/Nav.svelte';
+	import Footer from '$lib/Footer.svelte';
+	import PageTransition from '$lib/PageTransition.svelte';
+	export let key;
+</script>
 
 <div class="app-wrapper">
 	<Nav />
-	<div class="main-content">
-		<slot />
-	</div>
+	<main class="main-content px-20 py-7">
+		<PageTransition refresh={key}>
+			<slot />
+		</PageTransition>
+	</main>
 	<Footer />
 </div>
 
+<slot />
+
 <style>
 	.main-content {
-		padding: 0 2rem;
+		height: 90vh;
 	}
 	.app-wrapper {
 		font-family: Quantico;
